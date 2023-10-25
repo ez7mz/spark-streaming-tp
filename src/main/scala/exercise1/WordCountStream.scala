896a648695a1916150c8b4d8134b76406d6b3fef
ez7mz <mesrarhamza48@gmail.com>
2023-10-25 13:52:54 +0100

    feat: add exercise1

---
+++ src/main/scala/exercise1/WordCountStream.scala
@@ -0,0 +1,15 @@
+package exercise1
+
+import org.apache.spark._
+import org.apache.spark.streaming._
+
+/*
+  $ nc -lk 9999
+ */
+object WordCountStream {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+    // Your Code goes here
+  }
+
+}
