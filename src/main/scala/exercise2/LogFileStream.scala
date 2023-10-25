412ac896856660573d671e4bf2ba3d6490827247
ez7mz <mesrarhamza48@gmail.com>
2023-10-25 14:38:24 +0100

    feat: add exercise2

---
+++ src/main/scala/exercise2/LogFileStream.scala
@@ -0,0 +1,11 @@
+package exercise2
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{StructField, StructType}
+object LogFileStream {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+    // Your Code goes here (you will find the log file in the resources folder)
+  }
+}
