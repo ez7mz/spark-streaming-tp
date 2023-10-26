b98c127e228de9c12ac3edf5ec4647d50b9c2260
ez7mz <mesrarhamza48@gmail.com>
2023-10-26 14:23:39 +0100

    feat: exercise1 solution

--- src/main/scala/exercise1/WordCountStream.scala
+++ src/main/scala/exercise1/WordCountStream.scala
@@ -4,12 +4,28 @@ import org.apache.spark._
 import org.apache.spark.streaming._
 
 /*
-  $ nc -lk 9999
+  $ nc -lk 5000
  */
 object WordCountStream {
   def main(args: Array[String]): Unit = {
     println("Hello World")
-    // Your Code goes here
+    val conf = new SparkConf().setMaster("local[*]").setAppName("test")
+    val ssc = new StreamingContext(conf, Seconds(2))
+
+    val lines = ssc.socketTextStream("localhost", 5000)
+
+    val words = lines.flatMap(_.split(" "))
+
+    val tuples = words.map(word => (word, 1))
+
+    val result = tuples.reduceByKey(_ + _)
+
+    result.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+
+
   }
 
 }
